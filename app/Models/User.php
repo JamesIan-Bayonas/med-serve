a3a03e3208e70ec2b61ebdb29c2b0b6d3f7e9946
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2026-05-06 23:21:22 +0800

    feat: enhance role-based access control by integrating Spatie permissions and securing API routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,23 +6,25 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasRoles;
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    *
+    * @var list<string>
+    */
     protected $fillable = [
         'name',
         'email',
         'password',
-    ];
+        ];
 
     /**
      * The attributes that should be hidden for serialization.
